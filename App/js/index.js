a0a78a4ec2837dcfae601204b708c990bd93890f
diyidilei <35208524+diyidilei@users.noreply.github.com>
2019-08-28 09:55:38 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,117 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Panel")
+                .setHost(host,"xui_ui_panel4")
+                .setDock("none")
+                .setLeft("12.5em")
+                .setTop("5em")
+                .setWidth("37.5em")
+                .setHeight("28.333333333333332em")
+                .setCaption("数据表格")
+            );
+            
+            host.xui_ui_panel4.append(
+                xui.create("xui.UI.TreeGrid")
+                .setHost(host,"xui_ui_treegrid2")
+                .setDirtyMark(false)
+                .setLeft("0em")
+                .setTop("0em")
+                .setEditable(true)
+                .setRowHandler(false)
+                .setHeader([
+                    {
+                        "id":"name",
+                        "caption":"名称",
+                        "type":"label",
+                        "width":"9em"
+                    },
+                    {
+                        "id":"coordy",
+                        "caption":"坐标纬度",
+                        "type":"label",
+                        "width":"8em"
+                    },
+                    {
+                        "id":"coordx",
+                        "caption":"坐标经度",
+                        "type":"label",
+                        "width":"9.5em"
+                    },
+                    {
+                        "id":"handle",
+                        "caption":"操作",
+                        "type":"button",
+                        "width":"9em",
+                        "cellRenderer":""
+                    }
+                ])
+                .setRows([
+                    {
+                        "cells":[
+                            {
+                                "value":"海南级猎潜艇"
+                            },
+                            {
+                                "value":"19.153"
+                            },
+                            {
+                                "value":"156.888"
+                            },
+                            {
+                                "value":"绘制"
+                            }
+                        ]
+                    },
+                    {
+                        "cells":[
+                            {
+                                "value":"Group 44"
+                            },
+                            {
+                                "value":"46.444"
+                            },
+                            {
+                                "value":"18.999"
+                            },
+                            {
+                                "value":"绘制"
+                            }
+                        ]
+                    },
+                    {
+                        "cells":[
+                            {
+                                "value":"黄蜂级导弹艇"
+                            },
+                            {
+                                "value":"124.111"
+                            },
+                            {
+                                "value":"100.478"
+                            },
+                            {
+                                "value":"绘制"
+                            }
+                        ]
+                    }
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button53")
+                .setDirtyMark(false)
+                .setLeft("13.333333333333334em")
+                .setTop("35em")
+                .setWidth("10.666666666666666em")
+                .setHeight("3.6666666666666665em")
+                .setCaption("添加数据")
+                .setFontSize("14px")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
