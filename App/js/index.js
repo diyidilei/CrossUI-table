c85265e2e4ab8c1bd5b82698f33da8517f9dda56
diyidilei <35208524+diyidilei@users.noreply.github.com>
2019-08-28 11:03:34 +0800

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -24,7 +24,7 @@ xui.Class('App', 'xui.Module',{
                 xui.create("xui.UI.Panel")
                 .setHost(host,"xui_ui_panel4")
                 .setDock("none")
-                .setLeft("12.5em")
+                .setLeft("11.666666666666666em")
                 .setTop("1.6666666666666667em")
                 .setWidth("37.5em")
                 .setHeight("37.5em")
@@ -118,103 +118,6 @@ xui.Class('App', 'xui.Module',{
                 ])
             );
             
-            host.xui_ui_panel4.append(
-                xui.create("xui.UI.Dialog")
-                .setHost(host,"xui_ui_dialog2")
-                .setLeft("5.333333333333333em")
-                .setTop("14.666666666666666em")
-                .setZIndex(1003)
-                .setCaption("对话框")
-                .setMinBtn(false)
-            );
-            
-            host.xui_ui_dialog2.append(
-                xui.create("xui.UI.Input")
-                .setHost(host,"xui_ui_input1")
-                .setName("name")
-                .setDirtyMark(false)
-                .setLeft("-0.4166666666666667em")
-                .setTop("2.0833333333333335em")
-                .setWidth("18em")
-                .setLabelSize("8em")
-                .setLabelCaption("名称")
-            );
-            
-            host.xui_ui_dialog2.append(
-                xui.create("xui.UI.Input")
-                .setHost(host,"xui_ui_input2")
-                .setName("coordx")
-                .setDirtyMark(false)
-                .setLeft("-0.4166666666666667em")
-                .setTop("4.583333333333333em")
-                .setWidth("18em")
-                .setLabelSize("8em")
-                .setLabelCaption("坐标经度")
-            );
-            
-            host.xui_ui_dialog2.append(
-                xui.create("xui.UI.Input")
-                .setHost(host,"xui_ui_input3")
-                .setName("coordy")
-                .setDirtyMark(false)
-                .setLeft("-0.4166666666666667em")
-                .setTop("7.083333333333333em")
-                .setWidth("18em")
-                .setLabelSize("8em")
-                .setLabelCaption("坐标纬度")
-            );
-            
-            host.xui_ui_dialog2.append(
-                xui.create("xui.UI.HTMLButton")
-                .setHost(host,"xui_ui_htmlbutton4")
-                .setLeft("7.5em")
-                .setTop("11.666666666666666em")
-                .setWidth("8.25em")
-                .setHeight("3.0833333333333335em")
-                .setCaption("添加")
-                .onClick([
-                    {
-                        "desc":"储存数据",
-                        "type":"control",
-                        "target":"xui_ui_dialog2",
-                        "args":[
-                            "{page.xui_ui_dialog2.getFormValues()}",
-                            "global",
-                            "adddata"
-                        ],
-                        "method":"getFormValues",
-                        "redirection":"other:callback:call",
-                        "event":1
-                    },
-                    {
-                        "desc":"增加表格",
-                        "type":"control",
-                        "target":"xui_ui_treegrid2",
-                        "args":[
-                            "{global.adddata}",
-                            null,
-                            null,
-                            false
-                        ],
-                        "method":"insertRows",
-                        "conditions":[
-                            {
-                                "left":"{global.adddata}",
-                                "symbol":"non-empty",
-                                "right":""
-                            }
-                        ]
-                    },
-                    {
-                        "desc":"关闭弹出层",
-                        "type":"control",
-                        "target":"xui_ui_dialog2",
-                        "args":[ ],
-                        "method":"close"
-                    }
-                ])
-            );
-            
             append(
                 xui.create("xui.UI.Button")
                 .setHost(host,"xui_ui_button53")
@@ -278,10 +181,124 @@ xui.Class('App', 'xui.Module',{
             );
             
             append(
+                xui.create("xui.UI.Div")
+                .setHost(host,"xui_ui_div26")
+                .setLeft("34.166666666666664em")
+                .setTop("17.5em")
+                .setWidth("27.5em")
+                .setHeight("20.833333333333332em")
+            );
+            
+            host.xui_ui_div26.append(
+                xui.create("xui.UI.Dialog")
+                .setHost(host,"xui_ui_dialog12")
+                .setLeft("3.3333333333333335em")
+                .setTop("5.833333333333333em")
+                .setHeight("20.833333333333332em")
+                .setCaption("对话框")
+                .setMinBtn(false)
+            );
+            
+            host.xui_ui_dialog12.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input32")
+                .setName("coordx")
+                .setDirtyMark(false)
+                .setLeft("1.25em")
+                .setTop("5.416666666666667em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("坐标经度")
+            );
+            
+            host.xui_ui_dialog12.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input33")
+                .setName("name")
+                .setDirtyMark(false)
+                .setLeft("1.25em")
+                .setTop("2.9166666666666665em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("名称")
+            );
+            
+            host.xui_ui_dialog12.append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"xui_ui_input34")
+                .setName("coordy")
+                .setDirtyMark(false)
+                .setLeft("1.25em")
+                .setTop("7.916666666666667em")
+                .setWidth("18em")
+                .setLabelSize("8em")
+                .setLabelCaption("坐标纬度")
+            );
+            
+            host.xui_ui_dialog12.append(
+                xui.create("xui.UI.HTMLButton")
+                .setHost(host,"xui_ui_htmlbutton12")
+                .setLeft("7.916666666666667em")
+                .setTop("12.083333333333334em")
+                .setWidth("8.25em")
+                .setHeight("3.0833333333333335em")
+                .setCaption("添加")
+                .onClick([
+                    {
+                        "desc":"打开弹出层",
+                        "type":"control",
+                        "target":"xui_ui_dialog2",
+                        "args":[ ],
+                        "method":"show",
+                        "event":1
+                    },
+                    {
+                        "desc":"储存数据",
+                        "type":"control",
+                        "target":"xui_ui_dialog2",
+                        "args":[
+                            "{page.xui_ui_dialog2.getFormValues()}",
+                            "global",
+                            "adddata"
+                        ],
+                        "method":"getFormValues",
+                        "redirection":"other:callback:call",
+                        "event":1
+                    },
+                    {
+                        "desc":"增加表格",
+                        "type":"control",
+                        "target":"xui_ui_treegrid2",
+                        "args":[
+                            "{global.adddata}",
+                            null,
+                            null,
+                            false
+                        ],
+                        "method":"insertRows",
+                        "conditions":[
+                            {
+                                "left":"{global.adddata}",
+                                "symbol":"non-empty",
+                                "right":""
+                            }
+                        ]
+                    },
+                    {
+                        "desc":"关闭弹出层",
+                        "type":"control",
+                        "target":"xui_ui_dialog2",
+                        "args":[ ],
+                        "method":"close"
+                    }
+                ])
+            );
+            
+            append(
                 xui.create("xui.UI.HiddenInput")
-                .setHost(host,"xui_ui_hiddeninput1")
+                .setHost(host,"xui_ui_hiddeninput6")
                 .setName("handle")
-                .setValue("绘制")
+                .setValue("zuocao")
             );
             
             return children;
